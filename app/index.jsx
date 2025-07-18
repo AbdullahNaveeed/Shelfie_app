5157d539e9d3f6fc9f09ef4efb014def147e9f14
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2025-07-18 11:49:37 +0500

    Make a first card simple styling / revision

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Abdullah Naveed</Text>
+      <Text style={{ marginTop: 10, marginBottom: 10 }}>Reading List App</Text>
+      <View style={styles.card}>
+        <Text>This is a first Card</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0, 0, 0, 0.1)",
+  },
+});
